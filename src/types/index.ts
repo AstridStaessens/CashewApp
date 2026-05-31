866e89d9b12dd05d64fa43c30b6e34016b1aa81a
Astrid <astridstaessens@gmail.com>
2026-06-01 01:01:39 +0200

    types toegevoegd

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+
+
+
+export interface Expense {
+    id: string;
+    amount: number;
+    description: string;
+    date: Date;
+    category: string;
+    location: string;
+    createdAt: Date;
+    receiptPhotoUri: string;
+}
